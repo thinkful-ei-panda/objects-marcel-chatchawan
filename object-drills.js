1d1c0e239465f52f49c5a29011377d72fc125e04
jenthura <joshuasth@gmail.com>
2020-06-01 14:21:55 -0500

    added logs to attack/defense

--- object-drills.js
+++ object-drills.js
@@ -1,11 +1,12 @@
+/* eslint-disable strict */
 // 1. Object initializers and methods
 
 let loaf = {
-    flour: 300,
-    water: 210,
-    hydration: function() {
-        return this.water / this.flour * 100;
-    }
+  flour: 300,
+  water: 210,
+  hydration: function() {
+    return this.water / this.flour * 100;
+  }
 };
 
 console.log(loaf.hydration());
@@ -13,20 +14,20 @@ console.log(loaf.hydration());
 // 2. Iterating over an object's properties
 
 const strangeObj = {
-    foo: 'oof', 
-    bar: 'rab', 
-    fum: 'muf', 
-    quux: 'xuuq', 
-    spam: 'maps' };
+  foo: 'oof', 
+  bar: 'rab', 
+  fum: 'muf', 
+  quux: 'xuuq', 
+  spam: 'maps' };
 
 for (const property in strangeObj) {
-   console.log(`The ${property} is ${strangeObj[property]}`);
+  console.log(`The ${property} is ${strangeObj[property]}`);
 }
 
 // 3. Arrays in objects
 
 let food = {
-    meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
+  meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
 };
 
 console.log(food['meals'][3]);
@@ -34,58 +35,63 @@ console.log(food['meals'][3]);
 // 4. Arrays of objects
 
 const obj1 = {name: 'Bob Vance',
-jobTitle: 'Owner', boss: ''};
+  jobTitle: 'Owner', boss: ''};
 const obj2 = {name: 'Scruffy Scruffington',
-jobTitle: 'Janitor', boss: obj1};
+  jobTitle: 'Janitor', boss: obj1};
 const obj3 = {name: 'Marcel',
-jobTitle: 'Lead Developer', boss: obj1};
+  jobTitle: 'Lead Developer', boss: obj1};
 const obj4 = {name: 'Chatchawan', 
-jobTitle: 'Assistant to the Lead Developer', boss: obj3};
+  jobTitle: 'Assistant to the Lead Developer', boss: obj3};
 
 let userArray = [obj1, obj2, obj3, obj4];
 
 userArray.forEach(user =>
-    console.log(`This is ${user.name}, their job title is ${user.jobTitle}`)
+  console.log(`This is ${user.name}, their job title is ${user.jobTitle}`)
 );
 
 // 5. Properties that aren't there
 
-userArray.forEach(user => 
-    console.log(`${user.jobTitle} ${user.name} reports to ${user.boss}.`));
+userArray.forEach(user =>
+  console.log(`This is ${user.name}, their job title is ${user.jobTitle}`)
+);
 
 // 6. Cracking the code. 'craft block argon meter bells brown croon droop'
 let cipher = {
-      a: 2,
-      b: 3,
-      c: 4,
-      d: 5,
+  a: 2,
+  b: 3,
+  c: 4,
+  d: 5,
 };
 let decode = word => {
   let l = 'that';
-    l = word.charAt(0)[cipher];
-    console.log(l);
+  l = word.charAt(0)[cipher];
+  console.log(l);
 }; 
 console.log(decode('cat'));
 
 // 7. Factory Functions with LOTR
 
 function createCharacter(name, nickname, race, origin, attack, defense) {
-    return {
-        name,
-        nickname,
-        origin,
-        attack,
-        defense,
-        evaluateFight: function(enemy) {
-            if (enemy.defense > this.attack) {
-                let damageToEnemy = enemy.defense - this.attack;
-            }
-            if (this.defense > enemy.attack) {
-                let damageToSelf = this.defense - enemy.attack;
-            }
-            return `Your opponent takes ${damageToEnemy} damage and you receive ${damageToSelf} damage`;
-        }
-    };
+  return {
+    name,
+    nickname,
+    origin,
+    attack,
+    defense,
+    evaluateFight: function(enemy) {
+      let damageToEnemy = 0;
+      let damageToSelf = 0;
+      if (enemy.defense > this.attack) {
+        console.log(`Enemy defense is ${enemy.defense}, your attack is ${this.attack}`)
+        let damageToEnemy = enemy.defense - this.attack;
+      }
+      if (this.defense > enemy.attack) {
+        console.log(`Your defense is ${this.defense}, enemy attack is ${enemy.attack}`)
+        let damageToSelf = this.defense - enemy.attack;
+      }
+      return `Your opponent takes ${damageToEnemy} damage and you receive ${damageToSelf} damage`;
+    }
+  };
 }
 
 let character1 = createCharacter('Gandalf the White', 'gandalf', 'Wizard','Middle Earth', 10, 6)
@@ -93,3 +99,4 @@ let character2 = createCharacter('Bilbo Baggins', 'bilbo', 'Halfling','The Shire
 let character3 = createCharacter('Frodo Baggins', 'frodo', 'Halfling','The Shitre', 2, 1)
 
 console.log(character2.evaluateFight(character1))
+console.log(character1.evaluateFight(character2))
